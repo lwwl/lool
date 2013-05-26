d5e8c32e4e3ad036609b9ba21fe0c580de632864
eyehs <eyehs@naver.com>
2013-05-26 22:36:08 +0900

    Revert "Added helloworld"
    This reverts commit 86970f68b3a56bbace8efe82a043ddc82e42d8fa.

--- 001.helloworld/helloworld.js
+++
@@ -1,6 +0,0 @@
-var server = require('http');
-
-server.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type':'text/plain'});
-	res.end("Hello Node.js!\n");
-}).listen(3001, '0.0.0.0');
